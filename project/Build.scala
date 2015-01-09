450ea944abd25e201812a0e5bb66fb4e0a55fbb8
xuwei-k <6b656e6a69@gmail.com>
2015-01-09 22:26:24 +0700

    Scala 2.11.5

--- project/Build.scala
+++ project/Build.scala
@@ -20,7 +20,7 @@ object Generate extends Build {
 
   val commonSettins = Seq(
     scalaVersion := "2.10.4",
-    crossScalaVersions := scalaVersion.value :: "2.11.4" :: Nil,
+    crossScalaVersions := scalaVersion.value :: "2.11.5" :: Nil,
     scalacOptions ++= (
       "-deprecation" ::
       "-unchecked" ::
