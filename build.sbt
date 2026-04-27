c3a691b797473a0ee92e7e7a3ef3f9c99d0a423f
xuwei-k <6b656e6a69@gmail.com>
2026-04-27 09:32:13 +0900

    Update build file

--- build.sbt
+++ build.sbt
@@ -61,7 +61,7 @@ val commonSettings = Seq(
     tagRelease,
     ReleaseStep(
       action = { state =>
-        val extracted = Project extract state
+        val extracted = Project.extract(state)
         extracted.runAggregated(extracted.get(thisProjectRef) / (Global / PgpKeys.publishSigned), state)
       },
       enableCrossBuild = true
--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -6,31 +6,25 @@ object UpdateReadme {
 
   val moduleName = "play-json-extra"
 
-  val updateReadme = { state: State =>
+  val updateReadme: State => State = { state =>
     val extracted = Project.extract(state)
-    val scalaV = extracted get scalaBinaryVersion
-    val v = extracted get version
-    val org = extracted get organization
-    val snapshotOrRelease = if (extracted get isSnapshot) "snapshots" else "releases"
+    val v = extracted.get(version)
+    val org = extracted.get(organization)
     val readme = "README.md"
     val readmeFile = file(readme)
-    val SonatypeURL = "https://oss.sonatype.org/service/local/repositories"
-    val newReadme = Predef
-      .augmentString(IO.read(readmeFile))
-      .lines
+    val newReadme = IO
+      .readLines(readmeFile)
       .map { line =>
         val matchReleaseOrSnapshot = (line.contains("SNAPSHOT") == v.contains("SNAPSHOT")) && line.contains(moduleName)
         if (line.startsWith("libraryDependencies") && matchReleaseOrSnapshot && line.contains(" %% ")) {
           s"""libraryDependencies += "${org}" %% "${moduleName}" % "$v""""
         } else if (line.startsWith("libraryDependencies") && matchReleaseOrSnapshot && line.contains(" %%% ")) {
           s"""libraryDependencies += "${org}" %%% "${moduleName}" % "$v""""
-        } else if (line.contains(SonatypeURL) && matchReleaseOrSnapshot) {
-          s"- [API Documentation](${SonatypeURL}/${snapshotOrRelease}/archive/${org.replace('.', '/')}/${moduleName}_${scalaV}/${v}/${moduleName}_${scalaV}-${v}-javadoc.jar/!/index.html)"
         } else line
       }
       .mkString("", "\n", "\n")
     IO.write(readmeFile, newReadme)
-    val git = new sbtrelease.Git(extracted get baseDirectory)
+    val git = new sbtrelease.Git(extracted.get(baseDirectory))
     git.add(readme) ! state.log
     git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
     sys.process.Process("git diff HEAD^") ! state.log
